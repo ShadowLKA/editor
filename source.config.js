8a8e3f06542cdc1b5749a14f3c839f611b466a30
ShadowLKA <lukakandelaki12@gmail.com>
2026-01-13 19:52:14 +0400

    Update source.config.js
    Signed-off-by: ShadowLKA <lukakandelaki12@gmail.com>

--- source.config.js
+++ source.config.js
@@ -2,7 +2,7 @@
 
 export const SOURCE = {
   // Paste your external file link here (JSON or JS)
-  url: "https://heihssimnnilkowuxvfa.supabase.co/storage/v1/object/sign/Key/key/github-api-key.json?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNTlhNjM4Ni1lOGQyLTQ0MjAtOWM4YS1lNTc1ZDk4Y2M0YjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJLZXkva2V5L2dpdGh1Yi1hcGkta2V5Lmpzb24iLCJpYXQiOjE3NjgzMTQwNzUsImV4cCI6NDkyMTkxNDA3NX0.hkA0j2L4cq1yBe0--hG_NuzxwDG9pRoCB-sZr3eO6lI",
+  url: "https://heihssimnnilkowuxvfa.supabase.co/storage/v1/object/sign/Key/key/github-api-key.json?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hNTlhNjM4Ni1lOGQyLTQ0MjAtOWM4YS1lNTc1ZDk4Y2M0YjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJLZXkva2V5L2dpdGh1Yi1hcGkta2V5Lmpzb24iLCJpYXQiOjE3NjgzMTk1MTEsImV4cCI6NDkyMTkxOTUxMX0.Ovh3UE-HzBGWnxQTn0CALgzPByIMJ5dUHFGLgXdlOQM",
 
   // Local fallback JSON file
   fallbackJsonPath: "./github-api-key.json",
